feb49008301bd3305135bac2828fe167fd1f0cc8
i2305 <iva.vukovic695@gmail.com>
2023-06-16 12:13:04 +0200

    Update playerSelection and modify playRound function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let playerSelection=prompt("Choose your weapon: Rock, Paper or Scissors.");
-playerSelection=playerSelection.toLowerCase();
+function getPlayerChoice(){
+    let option=prompt("Choose your weapon: Rock, Paper or Scissors.");
+    option=option.toLowerCase();
+    return option;
+}
+
+let playerSelection=getPlayerChoice();
 console.log(playerSelection);
 
 function getComputerChoice(){
@@ -19,27 +24,46 @@ function getComputerChoice(){
 let computerSelection=getComputerChoice();
 console.log(computerSelection);
 
+let yourScore=0;
+let computerScore=0;
+let count=0;
+
 function playRound(playerSelection,computerSelection){
     if (playerSelection === computerSelection){
-        return "It's tied.Nobody wins!"
+        count+=1;
+        yourScore+=1;
+        computerScore+=1;
+        return "It is tied.Nobody wins!"
     }
     else if (playerSelection==="rock" && computerSelection==="scissors"){
+        yourScore+=1;
+        count+=1;
         return "You won, rock beats scissors!"
     }
     else if (playerSelection==="scissors" && computerSelection==="paper"){
+        yourScore+=1;
+        count+=1;
         return "You won, scissors beats paper!"
     }
     else if (playerSelection==="paper" && computerSelection==="rock"){
+        yourScore+=1;
+        count+=1;
         return "You won, paper beats rock!"
     }
     else if (playerSelection==="scissors" && computerSelection==="rock"){
+        computerScore+=1;
+        count+=1;
         return "You loose, rock beats scissors!"
     }
     else if (playerSelection==="paper" && computerSelection==="scissors"){
+        computerScore+=1;
+        count+=1;
         return "You loose, scissors beats paper!"
     }
     else if (playerSelection==="rock" && computerSelection==="paper"){
+        computerScore+=1;
+        count+=1;
         return "You loose, paper beats rock!"
     }
 }
-console.log(playRound(playerSelection, computerSelection));
+
