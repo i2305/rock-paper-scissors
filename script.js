fc200a7105d4f5e913366721bae529834e55bf51
i2305 <iva.vukovic695@gmail.com>
2023-07-10 15:09:19 +0200

    add html and modify script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const rockButton=document.querySelector(".rock");
+const paperButton=document.querySelector(".paper");
+const scissorsButton=document.querySelector(".scissors");
+const result=document.querySelector(".result");
+const playerScoreSpan=document.querySelector(".player-score");
+const computerScoreSpan=document.querySelector(".computer-score");
+
 function getComputerChoice(){
     let randomly=Math.floor(Math.random()*3);
     switch (randomly) {
@@ -12,64 +19,98 @@ function getComputerChoice(){
     }
 }
 
-let computerSelection=getComputerChoice();
-
-let playerSelection=prompt("Choose your weapon: Rock, Paper or Scissors.");
-playerSelection=playerSelection.toLowerCase();
-
 let playerScore=0;
 let computerScore=0;
 
 function playRound(playerSelection,computerSelection){
     if (playerSelection==="rock" && computerSelection==="scissors"){
         playerScore++;
-        return "You win, rock beats scissors!"
+        const p=document.createElement("p");
+        p.innerText="You win, rock beats scissors!"
+        result.appendChild(p)
     }
     else if (playerSelection==="scissors" && computerSelection==="paper"){
         playerScore++;
-        return "You win, scissors beats paper!"
+        const p=document.createElement("p");
+        p.innerText="You win, scissors beats paper!"
+        result.appendChild(p)
     }
     else if (playerSelection==="paper" && computerSelection==="rock"){
         playerScore++;
-        return "You win, paper beats rock!"
+        const p=document.createElement("p");
+        p.innerText= "You win, paper beats rock!"
+        result.appendChild(p)
     }
     else if (playerSelection==="scissors" && computerSelection==="rock"){
         computerScore++;
-        return "You loose, rock beats scissors!"
+        const p=document.createElement("p")
+        p.innerText= "You loose, rock beats scissors!"
+        result.appendChild(p)
     }
     else if (playerSelection==="paper" && computerSelection==="scissors"){
         computerScore++;
-        return "You loose, scissors beats paper!"
+        const p=document.createElement("p")
+        p.innerText="You loose, scissors beats paper!"
+        result.appendChild(p)
     }
     else if (playerSelection==="rock" && computerSelection==="paper"){
         computerScore++;
-        return "You loose, paper beats rock!"
+        const p=document.createElement("p")
+        p.innerText="You loose, paper beats rock!"
+        result.appendChild(p)
     }
     else if (playerSelection === computerSelection){
-        playerScore++;
-        computerScore++;
-        return "It is tied.Nobody wins!"
+        const p=document.createElement("p")
+        p.innerText= "It is tied.Nobody wins!"
+        result.appendChild(p)
+    }
+}
+const checkForWinner=(playerScore, computerScore)=>{
+    if (playerScore===5){
+        const h2=document.createElement("h2")
+        h2.classList.add("you-won")
+        h2.innerText=`Game over. Congratulations you beat the computer!\n Your score is ${playerScore}.\n Computer score is ${computerScore}.`
+        result.append(h2)
+    }
+    else if (computerScore===5){
+        const h2=document.createElement("h2")
+        h2.classList.add("computer-won")
+        h2.innerText=`Game over! Too bad, computer defeated you, better luck next time!\n Computer score is ${computerScore}.\n Your score is ${playerScore}.`
+        result.append(h2)
+    }
+    else if (playerScore===5 && computerScore===5){
+        const h2=document.createElement("h2")
+        h2.classList.add("tied")
+        h2.innerText=`Game over! It is tied, nobody wins!\n Your score is ${playerScore}.\n Computer score is ${computerScore}.`
+        result.append(h2)
     }
 }
 
-function game(){
-    for (let i=0; i<5; i++){
-        let playerSelection=prompt("Choose your weapon: Rock, Paper or Scissors.")
-        console.log(playerSelection);
-        let computerSelection=getComputerChoice();
-        console.log(computerSelection);
-        console.log(playRound(playerSelection,computerSelection));
-    }
-
-    if (playerScore>computerScore){
-        return `Game over. Congratulations you beat the computer!\n Your score is ${playerScore}.\n Computer score is ${computerScore}.`
-    }
-    else if (playerScore<computerScore){ 
-        return `Game over! Too bad, computer defeated you, better luck next time!\n Computer score is ${computerScore}.\n Your score is ${playerScore}.`
-    }
-    else {
-        return `Game over! It is tied, nobody wins!\n Your score is ${playerScore}.\n Computer score is ${computerScore}.`
-    }
+const updateScores=(playerScore, computerScore) => {
+    playerScoreSpan.innerText=`Player Score:${playerScore}`
+    computerScoreSpan.innerText=`Computer Score:${computerScore}`
 }
 
-console.log(game())
+rockButton.addEventListener("click", () => {
+    const computerSelection=getComputerChoice()
+    const playerSelection="rock"
+    playRound(playerSelection, computerSelection)
+    updateScores(playerScore, computerScore)
+    checkForWinner(playerScore, computerScore)
+})
+
+paperButton.addEventListener("click", () => {
+    const computerSelection=getComputerChoice()
+    const playerSelection="paper"
+    playRound(playerSelection, computerSelection)
+    updateScores(playerScore, computerScore)
+    checkForWinner(playerScore, computerScore)
+})
+
+scissorsButton.addEventListener("click", () => {
+    const computerSelection=getComputerChoice()
+    const playerSelection="scissors"
+    playRound(playerSelection, computerSelection)
+    updateScores(playerScore, computerScore)
+    checkForWinner(playerScore, computerScore)
+})
